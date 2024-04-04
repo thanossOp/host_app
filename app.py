5269258d523d435e36620ffd362bbb4e8d0becda
thanossOp <mr.jadhav721@gmail.com>
2024-04-04 14:40:15 +0530

    changes in app

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import pyttsx3
 app = Flask(__name__)
 
+def speak(response):
+    engine = pyttsx3.init()
+    voice = engine.getProperty("voices")
+    engine.setProperty("voice", voice[1].id)
+    engine.setProperty("rate", 180)
+    engine.setProperty("volume", 0.9)
+    engine.say(response)
+    engine.runAndWait()
+    
 @app.route('/')
 def hello_world():
+    speak("Hello Thanos Congratulations Your api is finally hosted . Now give us a Big treat.")
     return "Hii Thanos"
-
