22be369f9844ee0db8b7eae5b270da90bf845352
thanossOp <mr.jadhav721@gmail.com>
2024-04-04 14:30:45 +0530

    changes some

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
 from flask import Flask
-import pyttsx3
-
 app = Flask(__name__)
 
-def speak(response):
-    engine = pyttsx3.init()
-    voice = engine.getProperty("voices")
-    engine.setProperty("voice", voice[1].id)
-    engine.setProperty("rate", 180)
-    engine.setProperty("volume", 0.9)
-    engine.say(response)
-    engine.runAndWait()
+@app.route('/')
+def hello_world():
+    return "Hii Thanos"
 
-@app.route('/', methods=['GET'])
-def call_script():
-    speak("Hello Thanos! How are you? Congratulations! Your application is hosted successfully. Now give us a treat.")
-    return "Welcome Thanos on titan"
-
-if __name__ == "__main__":
-    app.run(debug=True)
